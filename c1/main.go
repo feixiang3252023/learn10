e97c03790b8d68ba7dad4688cf8cff7866b1d285
feixiang3252023 <feixiang3252023@163.com>
2024-08-07 14:06:00 +0800

    变量的声明和初始化

---
+++ c1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int  // 声明一个int型变量a
+	a = 10     // 给变量a赋值为10
+	var b = 20 // 声明一个int型变量b，并初始化为20
+	c := 30    // 短变量声明，同时声明和初始化变量c为30
+
+	fmt.Println(a, b, c) // 输出变量的值
+}
